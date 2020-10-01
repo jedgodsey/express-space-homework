c51a729a44f6c7bddf2098a5ad0386b8e4c2a4c9
jedgodsey <jed.godsey@gmail.com>
2020-09-30 21:41:33 -0700

    file structure set up

---
+++ controllers/missionControl.js
@@ -0,0 +1,3 @@
+const router = require('express').Router();
+
+const marsMissions = require('../models/marsMissions.js');
--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+// Set View Engine
+app.set('view engine', 'ejs');
+
 // run `npm install` to install dependencies in package.json
 
 // * Your mission is to complete the app
@@ -24,43 +27,7 @@ const port = 3000;
 
 // DATA - put into marsMissions.js file inside of a models folder, for module.exports
 // remember to require it in the server
-const marsMissions = [
-  {
-    name: "Curiosity",
-    launchDate: "26 Nov 2011",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Opportunity",
-    launchDate: "8 Jul 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Spirit",
-    launchDate: "10 Jun 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Sojourner",
-    launchDate: "4 Dec 1996",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Rosetta",
-    launchDate: "2 Mar 2004",
-    operator: "ESA",
-    missionType: "Gravity Assist",
-    img: ""
-  }
-];
+
 
 // INDEX Route
 // send data to 'missions/index.ejs' view
