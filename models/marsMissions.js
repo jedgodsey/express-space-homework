9fb977e8c09acfd89f2f2a4c568593dcd3681e8f
jedgodsey <jed.godsey@gmail.com>
2020-09-30 23:08:02 -0700

    finished hungry: images

--- models/marsMissions.js
+++ models/marsMissions.js
@@ -4,35 +4,35 @@ const marsMissions = [
       launchDate: "26 Nov 2011",
       operator: "NASA",
       missionType: "Rover",
-      img: ""
+      img: "https://www.nasa.gov/sites/default/files/images/551041main_pia14156-full_full.jpg"
     },
     {
       name: "Opportunity",
       launchDate: "8 Jul 2003",
       operator: "NASA",
       missionType: "Rover",
-      img: ""
+      img: "https://cnet4.cbsistatic.com/img/uMl1SuJhR98XvVKmBoAzHFEvm-o=/940x0/2019/03/01/2cbd20f0-0cab-44c4-96b6-a7f9244be15a/opportunity-rover.jpg"
     },
     {
       name: "Spirit",
       launchDate: "10 Jun 2003",
       operator: "NASA",
       missionType: "Rover",
-      img: ""
+      img: "https://astronomy.com/~/media/7F03303FC7394E17A0FA04A4D8EB079C.jpg"
     },
     {
       name: "Sojourner",
       launchDate: "4 Dec 1996",
       operator: "NASA",
       missionType: "Rover",
-      img: ""
+      img: "https://mars.nasa.gov/MPF/roverpwr/blue_up1.jpg"
     },
     {
       name: "Rosetta",
       launchDate: "2 Mar 2004",
       operator: "ESA",
       missionType: "Gravity Assist",
-      img: ""
+      img: "https://www.dlr.de/content/en/images/2014/1/philae-landing-on-comet_13339.jpg?__blob=normal&v=13"
     }
   ];
 
--- server.js
+++ server.js
@@ -8,22 +8,15 @@ const bodyParser = require('body-parser')
 app.set('view engine', 'ejs');
 
 // use Body parser
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.urlencoded({extended: false}));
 
 // Set controller
-const missionControl = require('./controllers/missionControl.js')
+const missionControl = require('./controllers/missionControl.js');
 
 // Use controller
 app.use('/', missionControl);
 
-// Use con
-// * Your mission is to complete the app
-// * The app will need routes for index and show
-// * The app will need views for index and show
-//
-// * MAIN GOAL:
-// * User should be able to click on a mission’s name on the index page, and be taken to that mission’s show page
-//
+
 // * Bonus/Hungry for More: add images to the data and have them display (google how)
 // * Bonus/Hungry for More: add static css to style the pages (google how)
 
